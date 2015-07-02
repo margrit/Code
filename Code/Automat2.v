d815afec49a98e0351b3ab53f5c46f14a4035f28
margrit <mdittman@uni-potsdam.de>
2015-07-02 10:17:20 +0200

    DEA dazu

--- Code/Automat2.v
+++ Code/Automat2.v
@@ -55,6 +55,7 @@ Inductive Automat: Type :=
 
 (* Automat allgemein *)
 (* http://stackoverflow.com/questions/22475373/modelisation-of-an-automaton-with-coq *)
+
 Record Automat: Type :=
   mk_automat{
   states: Type;
@@ -64,11 +65,10 @@ Record Automat: Type :=
   transitions: states -> alphabet -> list states
   }.
 
-
 Definition testNEA : Automat := mk_automat (Finite_Set 2) (Finite_Set 2) 
                                 (F1 1) ((FS 1 (F1 0))::nil) (fun x y => nil).
 
-(* ?? Finite_Set 2 steht für -- Warum 2 -- ?? *)
+(* Finite_Set 2 steht für -- Warum 2 -- Anzahl der Zustände und Länge des Alphabets *)
 
 Definition mk_NEA (s: nat) (a: nat) 
            (ini: (Finite_Set s)) 
@@ -76,18 +76,61 @@ Definition mk_NEA (s: nat) (a: nat)
            (trans : ((Finite_Set s) -> (Finite_Set a) -> (list (Finite_Set s)))) : 
            Automat := mk_automat (Finite_Set s) (Finite_Set a) ini final trans.  
 
+(* Definition eines NEA, der nichts macht *)
+
 Definition test2NEA : Automat := mk_NEA 3 4 (F1 2) nil (fun q x => nil).
+
+(* Alternative Definitions eines DEAs *)
+
+Record DAutomat: Type :=
+  mk_dauto{
+  dstates: Type;
+  dalphabet: Type;
+  dinit: dstates;
+  dfinal: list dstates;
+  dtrans: dstates -> dalphabet -> dstates
+  }.
+
+Definition mk_DEA (s : nat) (a : nat)
+           (ini : (Finite_Set s))
+           (final : (list(Finite_Set s)))
+           (trans : ((Finite_Set s) -> (Finite_Set a) -> (Finite_Set s))):
+           DAutomat := mk_dauto (Finite_Set s) (Finite_Set a) ini final trans.
+
+(* optimierte Teilmengenkonstruktion
+   - Umwandlung NEA => DEA 
+   - nicht alle Zustände sind erreichbar
+   - Epsilon-Hülle darstellen
+   **
+   - iterativ mit (d)trans zu konstruieren
+   ** start : Q_0 := {qDEA} = {e-Hülle(q0)}
+   ** go : Q_i+1 := Q_i union {dtrans(S,a)| S : Q (erreichbare Zustände), a : alphabet}
+   ** stop := Q_i+1 = Q_i => Q_DEA := Q_i
+*)
+
+(* Startzustand und alle Zustände, die von dort aus
+   durch Epsilon-Übergänge erreichet werden können 
+   - brauch man ne Abbruchbedingung (unendlich viele E-Übergänge - eher nicht) 
+*)
+(*
+Fixpoint start (s : nat) (QDEA : ) : Finite_Set := match QDEA with
+               |ini => QDEA  
+               end.
+*)
+Fixpoint go (s : nat) (a : nat) (QDEA : ) {Q_i+1 = Q_i} : Type := match ... with
+               | init => QDEA
+               | trans
+               |
+               end.
+
 
 (* Definition Injektivitaet 
    - Elemente aus Menge A werden eindeutig, durch f, in Menge B abgebildet *)
-Definition injective (f: A -> B) :=
-  forall x y : A, f x = f y -> x=y.
+Definition injective (f: A -> B) := forall x y : A, f x = f y -> x=y.
 
-Definition surjective (f: A -> B) :=
-  forall y : B, exists x : A, f x =y.
+Definition surjective (f: A -> B) := forall y : B, exists x : A, f x =y.
 
-Definition notInjective (f: A -> B) :=
-  exists x y : A, (f x = f y /\ (not (x = y))).
+Definition notInjective (f: A -> B) := exists x y : A, (f x = f y /\ (not (x = y))).
 
 End definitions.
 Section lemmas.
@@ -111,7 +154,7 @@ Qed.
 
 (* notInjImplNot_Inj arbeitet auf Set und nicht auf Finite_Set *)
 
-Lemma Not_InjImplNotInj1 : forall (A B : Set)(f : A -> B),
+(* Lemma Not_InjImplNotInj1 : forall (A B : Set)(f : A -> B),
                            (not (injective A B f)) -> (notInjective A B f).
 Proof.
   intros.
@@ -121,7 +164,17 @@ Proof.
   unfold not.
   induction H.
   intros.
-  apply H.
+*)  
+
+(* Klassische vs. konstruktivistischer Logik
+   - Welche Vorraussetzungen müssen noch getroffen werden um entscheidbar zu sein?
+   - Die 2. Ricktung bekommt man hin, wenn man mehr Einschränkungen auf endliche Mengen
+     definiert. 
+   - Beide Varianten mal aufschreiben und sowohl die Hin- als auch die Rückrichtung 
+     zeigen.
+   - Wie geht man mit der doppelten Negation um?
+*)
+
 
 Lemma Not_InjImplNotInj : forall (n m : nat)  (f : (Finite_Set n) -> (Finite_Set m)) , 
                           (not (injective (Finite_Set n) (Finite_Set m) f)) 
@@ -134,14 +187,16 @@ Proof.
   induction n.
   elim H.
   intros.
-  
-  (* injective_projections. *)
+
+(* z.z. dass Finite_Set 0 leer ist, dann müsste man weiter kommen.
+   Zur Not ein Lemma mit aufnehmen, dass genau das zeigt. *)
+  induction x.
   
 
 (*x=y->fx=fy kann man mit f_equal oder injection zeigen. Problem, man kann nicht sagen 
 ob f terminiert, daher geht es nicht so einfach anders herum. *)
   
-  
+
 Abort.
 
 
