8f10c5009369e04f3835ec110e252bcb1c0bff7f
Margrit Dittmann <margrit.dittmann@uni-potsdam.de>
2016-06-04 14:09:27 +0200

    Strukturen überarbeitet

--- Code/wortspiel.v
+++ Code/wortspiel.v
@@ -1,25 +1,31 @@
+Require Import List.
 (* Nur Spielerei: *)
 Inductive Sigma := h : Sigma | a : Sigma | l : Sigma | o : Sigma.
 
+(*Definition von Wörtern*)
 
 Inductive word {A : Type} : Type:= 
-| eps : @word A 
-| snoc : @word A -> A -> @word A.
+  | eps   : @word A
+  | snoc : @word A -> A -> @word A.
 
-(* Zu Vergleich: *)
+(* Berechnung der Wortlaenge *)
+Fixpoint word_length {A : Type} (w : @word A) : nat :=
+  match w with
+    | eps          => 0
+    | snoc w' x => S (word_length w')
+  end.
 
-Print list.
-Print word.
+Eval compute in (word_length (snoc (snoc (snoc (snoc (snoc eps h) a) l) l) o)).
 
 (*Verknüpfung zweier Wörter*)
 Fixpoint concat_word {A : Type} (w1 w2 : @word A) : @word A :=
-match w2 with 
-| eps => w1
-| snoc w x => snoc (concat_word w1 w) x
-end.
+  match w2 with
+    | eps => w1
+    | snoc w x => snoc (concat_word w1 w) x
+  end.
 
 Eval compute in (concat_word (snoc (snoc (snoc eps h) a) l) (snoc (snoc eps l) o)).
-Print "++".
+
 (*Eigenschaften von concat_word*)
 Lemma concat_word_nil {A : Type} (w : @word A) : concat_word eps w = w.
 Proof.
@@ -42,26 +48,14 @@ Proof.
     reflexivity.
 Defined.
 
-(* Berechnung der Wortlaenge *)
-Fixpoint word_length {A : Type} (w : @word A) : nat :=
-match w with
-| eps          => 0
-| snoc w' x => S (word_length w')
-end.
-
-Eval compute in (word_length (snoc (snoc (snoc (snoc (snoc eps h) a) l) l) o)).
-
 (* Ein Wort umdrehen: *)
 Fixpoint word_reverse {A : Type} (w : @word A) : @word A  :=
-match w with
-| eps           => eps
-| snoc w' x  => concat_word (snoc eps x) (word_reverse w')
-end.
+  match w with
+    | eps           => eps
+    | snoc w' x  => concat_word (snoc eps x) (word_reverse w')
+  end.
 
 (*Eigenschaften von word_reverse*)
-
-(*Lemma word_reverse_nil  {A : Type} (w : @word A) : snoc*)
-
 Lemma word_reverse_concat_word {A : Type} (w1 w2 : @word A) :
       word_reverse (concat_word w1 w2) = concat_word (word_reverse w2) (word_reverse w1).
 Proof.
@@ -94,21 +88,26 @@ Defined.
 
 Eval compute in (word_reverse (snoc (snoc (snoc (snoc (snoc eps h) a) l) l) o)).
 
+(*Eigenschaften von concat - aus wortspiel2.v*)
+
+(*Eigenschaften von rev - aus wortspiel2.v*)
+
+
 (* Ein Wort in eine Liste umwandeln: *)
 Fixpoint word_to_list {A : Type} (w : @word A) : list A :=
-match w with
-| eps           => nil
-| snoc w' x  => cons x (word_to_list w')
-end.
+  match w with
+    | eps           => nil
+    | snoc w' x  => cons x (word_to_list w')
+  end.
 
 Eval compute in (word_to_list (snoc (snoc (snoc (snoc (snoc eps h) a) l) l) o)).
 
 (* Eine Liste in ein Wort umwandeln: *)
 Fixpoint list_to_word {A : Type} (l : list A) : @word A :=
-match l with
-| nil           => eps
-| cons x l'  => snoc (list_to_word l') x
-end.
+  match l with
+    | nil           => eps
+    | cons x l'  => snoc (list_to_word l') x
+  end.
 
 Eval compute in (list_to_word (cons h(cons a(cons l nil)))).
 
@@ -133,10 +132,6 @@ induction w.
     rewrite IHw.
     reflexivity.
 Qed.
-
-Require Import List.
-
-Print rev.
 
 (*Ein Wort in eine Liste umwandeln unter Beachtung der Reihenfolge.*)
 Definition word_to_list'' {A : Type} (w : @word A) : list A := rev (word_to_list w).
@@ -200,9 +195,11 @@ Qed.
 Lemma list_word_list'' {A : Type} (l : list A) : word_to_list'' (list_to_word'' l) = l.
 Proof.
 unfold word_to_list''.
+unfold list_to_word''.
 induction l.
   - simpl.
     reflexivity.
-  - 
+  - simpl.
+    rewrite <- IHl.
 
 Lemma word_list_word'' {A : Type} (w : @word A) : list_to_word'' (word_to_list'' w) = w.
