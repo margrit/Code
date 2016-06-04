a779252a88723e83370f199af595e4a3ba73e454
Margrit Dittmann <margrit.dittmann@uni-potsdam.de>
2016-06-04 13:47:54 +0200

    l_t_w_rec und w_t_l_rec noch nicht korrekt

--- Code/wortspiel.v
+++ Code/wortspiel.v
@@ -11,7 +11,7 @@ Inductive word {A : Type} : Type:=
 Print list.
 Print word.
 
-
+(*Verknüpfung zweier Wörter*)
 Fixpoint concat_word {A : Type} (w1 w2 : @word A) : @word A :=
 match w2 with 
 | eps => w1
@@ -21,22 +21,26 @@ end.
 Eval compute in (concat_word (snoc (snoc (snoc eps h) a) l) (snoc (snoc eps l) o)).
 Print "++".
 (*Eigenschaften von concat_word*)
-Lemma concat_word_nil {A : Type} (w : @word A) : concat_word w eps = w.
+Lemma concat_word_nil {A : Type} (w : @word A) : concat_word eps w = w.
 Proof.
   induction w.
   - simpl.
     reflexivity.
   - simpl.
+    rewrite IHw.
     reflexivity.
 Defined.
 
-(*Lemma concat_word_associative {A : Type} (w1 w2 w3 : @word A) :
+Lemma concat_word_associative {A : Type} (w1 w2 w3 : @word A) :
   concat_word (concat_word w1 w2) w3 = concat_word w1 (concat_word w2 w3).
 Proof.
-  induction w1.
-  -
-*)
-(* Zur Uebung: *)
+  induction w3.
+  - simpl.
+    reflexivity.
+  - simpl.
+    rewrite IHw3.
+    reflexivity.
+Defined.
 
 (* Berechnung der Wortlaenge *)
 Fixpoint word_length {A : Type} (w : @word A) : nat :=
@@ -58,19 +62,20 @@ end.
 
 (*Lemma word_reverse_nil  {A : Type} (w : @word A) : snoc*)
 
-(*Lemma word_reverse_concat_word {A : Type} (w1 w2 : @word A) :
-      word_reverse (concat_word w1 w2) = concat_word (word_reverse w1) (word_reverse w2).
+Lemma word_reverse_concat_word {A : Type} (w1 w2 : @word A) :
+      word_reverse (concat_word w1 w2) = concat_word (word_reverse w2) (word_reverse w1).
 Proof.
-  induction w1.
+induction w2.
   - assert (word_reverse eps = @eps A).
     + simpl; reflexivity.
     + rewrite H.
-    pose (concat_word_nil (word_reverse w2)).
-...
-    rewrite e.
-    simpl; reflexivity.
+       simpl.
+       pose (concat_word_nil (word_reverse w1)) as con_nil.
+       rewrite con_nil.
+       reflexivity.
   - simpl.
     rewrite IHw2.
+    apply eq_sym.
     apply concat_word_associative.
 Defined.
 
@@ -80,13 +85,12 @@ Proof.
   - simpl.
     reflexivity.
   - simpl.
-    pose (word_reverse_concat_word (word_reverse w) snoc nil a).
+    pose (word_reverse_concat_word (snoc eps a0) (word_reverse w) ).
     rewrite e.
     rewrite IHw.
     simpl.
     reflexivity.
 Defined.
-*)
 
 Eval compute in (word_reverse (snoc (snoc (snoc (snoc (snoc eps h) a) l) l) o)).
 
@@ -137,6 +141,18 @@ Print rev.
 (*Ein Wort in eine Liste umwandeln unter Beachtung der Reihenfolge.*)
 Definition word_to_list'' {A : Type} (w : @word A) : list A := rev (word_to_list w).
 Eval compute in (word_to_list'' (snoc (snoc (snoc (snoc (snoc eps h) a) l) l) o)).
+
+(*Idee: Wort erst in eine Liste umwandeln und dann die Reihenfolge wieder zurück ändern.*)
+(*Fixpoint word_to_list_rec {A: Type} (w : @word A) : list A :=
+  match w with
+  | eps           => nil
+  | snoc w' x  => match w' with
+      | eps => nil
+      | snoc w'' x => concat (cons x nil) (word_to_list_rec w')
+    end
+end.
+*)
+
 Definition word_to_list''' {A : Type} (w : @word A) : list A := word_to_list (word_reverse w).
 Eval compute in (word_to_list''' (snoc (snoc (snoc (snoc (snoc eps h) a) l) l) o)).
 
@@ -151,6 +167,18 @@ Eval compute in (word_to_list' (snoc (snoc (snoc (snoc (snoc eps h) a) l) l) o))
 (*Eine Liste in ein Wort umwandeln unter Beachtung der Reihenfolge.*)
 Definition list_to_word'' {A : Type} (l : list A) : @word A := word_reverse (list_to_word l).
 Eval compute in (list_to_word'' (cons h(cons a(cons l nil)))).
+
+Fixpoint list_to_word_rec {A : Type} (l : list A) : @word A :=
+match l with
+  | nil           => eps
+  | cons x l'  => match l' with
+      | nil => eps
+      | cons x l'' => concat_word (snoc eps x) (word_reverse (list_to_word_rec l'))
+    end
+end.
+
+Eval compute in (list_to_word_rec (cons h(cons a(cons l (cons l nil))))).
+
 Definition list_to_word''' {A : Type} (l : list A) : @word A := list_to_word (rev l).
 Eval compute in (list_to_word''' (cons h(cons a(cons l nil)))).
 
