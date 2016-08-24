e78c1c3caa1440bbae614b956c7a924dd116cc8e
Tim Richter <tim@cs.uni-potsdam.de>
2016-08-24 14:03:30 +0200

    wip

--- Code/Finite_Set2.v
+++ Code/Finite_Set2.v
@@ -1,6 +1,9 @@
 Require Import Fin.
 Require Import Program.
 
+
+(* type isomorphisms a.s.o. *)
+
 Record Iso (A B : Type) : Type :=
   MkIso {
       to : A -> B;
@@ -42,6 +45,8 @@ Proof.
        exact (bccb c).
 Defined.      
 
+(* finite types *)
+
 Record Finite (X : Type) : Type :=
   MkFinite {
       card : nat;
@@ -51,11 +56,20 @@ Record Finite (X : Type) : Type :=
 Definition FiniteType : Type :=
   sigT Finite.
 
+(* for any cardinality, we have the standard finite type
+   of that cardinality *)
+
 Definition FinFinite (card : nat) : FiniteType.
 Proof.
   unfold FiniteType.  
   exact (existT Finite (@Fin.t card) (MkFinite (@Fin.t card) card (idIso (@Fin.t card)))).
 Defined.
+
+(* recursion and induction "principles"...
+   idea: things about finite types are determined
+         if they are determined for the standard finite types
+   very preliminary
+ *)
 
 Lemma recFinite0 (B : Type) (onFin : forall (card : nat), B) (X : FiniteType) : B.
 Proof.
@@ -89,3 +103,31 @@ Lemma indFinite (B : nat -> Type)
                 {X : FiniteType}
                 (x : (projT1 X)) : recFinite Type ...
 *)
+
+(* let's do the operations we need "by hand" *)
+
+Fixpoint optionFin {n : nat} (f : @Fin.t (S n)) : option (Fin.t n) :=
+match f with
+| F1 => None
+| FS f' => Some f'
+end.
+
+Fixpoint optionFinite (X : FiniteType) : FiniteType.
+Proof.
+  destruct X as [X [card iso]].
+  destruct iso as [xf fx xffx fxxf].
+  assert (oxsf : option X -> @Fin.t (S card)).
+  - intro ox.
+    induction ox.
+    + exact (FS (xf a)).
+    + exact F1.
+  - assert (sfox : @Fin.t (S card) -> option X).
+    + intro sf.
+      pose (optionFin sf) as osf.
+      induction osf.
+      * exact (Some (fx a)).
+      * exact None.
+    + assert (sfoxoxsf : forall (ox: option X), sfox (oxsf ox) = ox).
+      * intro ox.
+        induction ox.
+        { - compute.
