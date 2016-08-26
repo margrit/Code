193230fc9e612fd2ab03b521eed1031d3733e973
Tim Richter <tim@cs.uni-potsdam.de>
2016-08-26 09:34:14 +0200

    wip

--- Code/Finite_Set2.v
+++ Code/Finite_Set2.v
@@ -1,7 +1,6 @@
 Require Import Fin.
 Require Import Program.
 
-
 (* type isomorphisms a.s.o. *)
 
 Record Iso (A B : Type) : Type :=
@@ -15,7 +14,7 @@ Record Iso (A B : Type) : Type :=
 (* identity isomorphism *)
 Lemma idIso (A : Type) : Iso A A.
 Proof.    
-  exact (MkIso A A id id (fun b => eq_refl) (fun b => eq_refl)).  
+  exact (MkIso _ _ id id (fun b => eq_refl) (fun b => eq_refl)).  
 Defined.
 
 (* inversion of isomorphisms *)
@@ -23,14 +22,14 @@ Lemma symIso (A B : Type) : Iso A B -> Iso B A.
 Proof.
   intro isoAB.
   destruct isoAB as [to from toFrom fromTo].
-  exact (MkIso B A from to fromTo toFrom).
+  exact (MkIso _ _ from to fromTo toFrom).
 Defined.
 
-(* we even have: *)
+(* we even have (but never use..) *)
 Lemma symIsoIso (A B : Type) : Iso (Iso A B) (Iso B A).
 Proof.
   apply (MkIso _ _ (symIso A B) (symIso B A));
-  intro isoab;destruct isoab; compute; reflexivity.
+  intro isoab; destruct isoab; compute; reflexivity.
 Defined.
   
 (* composition of isomorphisms *)
@@ -44,6 +43,13 @@ Proof.
   + intro a; compute; rewrite (cbbc (ab a)); rewrite (baab a); reflexivity.
 Defined.      
 
+(* "flipped" version just for convenience *)
+Lemma transIso' (A B C : Type) : Iso A B -> Iso B C -> Iso A C.
+Proof.
+  intros isoab isobc.  
+  exact (transIso _ _ _ isobc isoab).
+Defined.
+  
 (*
 (* with function extensionality, we would even have: *)
 Lemma transIsoIso (A B C : Type) (isoBC : Iso B C) : Iso (Iso A B) (Iso A C).
@@ -254,16 +260,69 @@ Proof. (* couldn't we deduce this from sumIsoLeft and symmetry properties ?*)
     - trivial.
     - compute; rewrite (yxxy x); reflexivity.
 Defined.
-      
+
+Lemma falseFromFin0 (x : Fin.t 0) : False.
+Proof.
+  dependent induction x.
+Defined.
+
+Lemma finZeroIsoFalse : Iso False (Fin.t 0).
+Proof.
+  apply (MkIso _ _ (False_rect (t 0)) falseFromFin0).
+  + intro x; dependent induction x.
+  + intro a; apply False_rect; exact a.
+Defined.
+
+Lemma sumFalseIso (X : Type) : Iso (False + X) X.
+Proof.
+  apply (MkIso _ _ (univSum (False_rect X) id) inr).
+  + intro x; compute; reflexivity.
+  + intro fx; induction fx as [f | x].
+    - apply False_rect; exact f.  
+    - compute; reflexivity.
+Defined.
+
+(* Definition OfCard (n : nat) (X : Type) : Type := Iso X (Fin.t n). *)
+
+Lemma sumFiniteLemma (n m : nat) : forall (X Y : Type),
+                                   (Iso X (Fin.t n)) -> (Iso Y (Fin.t m)) ->
+                                   (Iso (X + Y) (Fin.t (n + m))).
+Proof.
+  induction n.
+  + intros X Y isoXt0 isoYtm.
+    compute.
+    pose (transIso _ _ _ (symIso _ _ finZeroIsoFalse) isoXt0) as isoXF.
+    pose (sumIsoLeft _ _ Y isoXF) as isoSumXYSumFY.   
+    apply (transIso' _ _ (t m) isoSumXYSumFY).
+    apply (transIso' _ _ (t m) (sumFalseIso Y)).
+    assumption.    
+  + intros X Y isoXtSn isoYtm.
+    pose (sumIsoLeft _ _ Y isoXtSn) as isoSumXYSumtSnY.
+    apply (transIso' _ _ (t (S n + m)) isoSumXYSumtSnY).
+    clear isoXtSn isoSumXYSumtSnY X.
+    pose (sumIsoLeft _ _ Y (@optionFinIso n)) as isoSumtSnYSumOtn.
+    apply (transIso' _ _ (t (S n + m)) isoSumtSnYSumOtn).
+    clear isoSumtSnYSumOtn.
+    apply (transIso' _ _ (t (S n + m)) (optionSumIso (t n) Y)).
+    pose (IHn (t n) Y (idIso (t n)) isoYtm) as isoSumtnYtnm.
+    apply (transIso' _ _ (t (S n + m)) (optionIso isoSumtnYtnm)).
+    clear isoYtm isoSumtnYtnm Y.
+    apply (transIso' _ _ (t (S n + m)) (symIso _ _ (@optionFinIso (n + m)))).
+    exact (idIso (t (S n + m))).    
+Defined.
+    
 Lemma sumFinite (X Y : Type) (Xfin : Finite X) (Yfin : Finite Y) : Finite (X + Y).
 Proof.
-  destruct Xfin as [card isoX].
-  induction card.
-
-  
+  destruct Xfin as [cardX isoX].
+  destruct Yfin as [cardY isoY].
+  apply (MkFinite _ (cardX + cardY)).
+  apply (sumFiniteLemma cardX cardY X Y isoX isoY).
+Defined.
+      
 Fixpoint addFinite (X Y : FiniteType) : FiniteType.
 Proof.
-   destruct X as [X [cardX isoX]].
-   induction cardX.
-   + exact Y.
-   + 
+   destruct X as [X Xfin].
+   destruct Y as [Y Yfin].
+   exact (existT Finite (sum X Y) (sumFinite X Y Xfin Yfin)).
+Defined.
+   
