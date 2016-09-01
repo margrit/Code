ed97ad01a7abdf4f3f390d2ebb0aefef5ae91fd7
Margrit Dittmann <margrit.dittmann@uni-potsdam.de>
2016-09-01 15:10:04 +0200

    angefangen

---
+++ Code/Beispielautomat.v
@@ -0,0 +1,17 @@
+Load DFA_Def.
+(*Inductive Sigma := h : Sigma | a : Sigma | l : Sigma | o : Sigma.*)
+
+Inductive Q' := on : Q' | off : Q'.
+Inductive Sigma' := press : Sigma'.
+
+Definition Q'_size := 2.
+Definition Sigma'_size := 1.
+
+(*Definition, welche Übergänge möglich sind.*)
+Fixpoint next_state (p : Q') (a : Sigma') : Q' :=
+match p with
+  | on  => off
+  | off  => on
+end.
+
+(*Abbildungen to, from, toFrom und fromTo zu Fin und isFinite *)
