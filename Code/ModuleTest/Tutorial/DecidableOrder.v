7debf87c2f7eff16ed568dff544df69919b7690f
Tim Richter <tim@cs.uni-potsdam.de>
2016-09-07 14:43:14 +0200

    DecidableOrder typecheckt (bei mir)

--- Code/ModuleTest/Tutorial/DecidableOrder.v
+++ Code/ModuleTest/Tutorial/DecidableOrder.v
@@ -13,9 +13,9 @@ Open Scope order_scope.
 Axiom le_refl : ∀ x, x ≤ x.
 Axiom le_antisym : ∀ x y, x ≤ y → y ≤ x → x = y.
 Axiom le_trans : ∀ x y z, x ≤ y → y ≤ z → x ≤ z.
-Axiom le_total : ∀ x y, {x ≤ y} \/ {y ≤ x}.
+Axiom le_total : ∀ x y, (x ≤ y) + (y ≤ x).
 
-Parameter le_dec : ∀ x y, {x ≤ y} + {¬ x ≤ y}. 
+Parameter le_dec : ∀ x y, (x ≤ y) + (¬ x ≤ y). 
 End Sig.
 
 
@@ -34,23 +34,42 @@ Proof.
 intros.
 unfold min.
 destruct (le_dec x y).
-tauto.
-destruct (le_total x y).
-absurd (le x y); assumption.
-tauto.
++ exact (X l).
++ destruct (le_total x y).
+  - absurd (le x y).
+    * exact n.
+    * exact l.
+  - exact (X0 l). 
 Qed.
 
 Lemma min_glb : ∀ x y z, z ≤ x -> z ≤ y -> z ≤ min x y.
 Proof.
 intros.
-apply case_min; tauto.
+apply case_min.
++ intro; exact H.  
++ intro; exact H0.
 Qed.
 
 Lemma min_sym : ∀ x y, min x y = min y x.
 Proof.
 intros.
 set (H:=le_antisym).
-do 2 apply case_min; auto.
+(* das schliesst den Beweis ab:
+do 2 apply case_min; auto.x *)
+(* die Tippeltappeltour geht so : *)
+apply case_min.
++ intro.
+  apply case_min.
+  - intro.
+    exact (H _ _ H0 H1).
+  - intro.
+    exact (H _ _ (le_refl x) (le_refl x)).
++ intro.
+  apply case_min.
+  - intro.
+    exact (H _ _ (le_refl y) (le_refl y)).
+  - intro.
+    exact (H _ _ H0 H1).
 Qed.
 
 Lemma min_left : ∀ x y, min x y ≤ x.
